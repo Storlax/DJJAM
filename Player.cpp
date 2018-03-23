722864534041c5040dba9ca5ca5d50a03dcea64b
nicolasmarzolini <nicolasmarzolini@ufl.edu>
2018-03-23 19:18:38 -0400

    Pussy

--- Player.cpp
+++ Player.cpp
@@ -5,6 +5,6 @@
 #include "Player.h"
 
 Player::Player(){
-    t1.loadFromFile("C:/Users/miami/CLionProjects/DJJAM/cmake_modules/Images/lol.png");
-    t2.loadFromFile("C:/Users/miami/CLionProjects/DJJAM/cmake_modules/Images/lolFlipped.png");
+    t1.loadFromFile("C:/Users/miami/CLionProjects/DJJAM/cmake_modules/Images/doodle copy.png");
+    t2.loadFromFile("C:/Users/miami/CLionProjects/DJJAM/cmake_modules/Images/doodleCopyLeftRight.png");
 }
--- game.cpp
+++ game.cpp
@@ -1,4 +1,4 @@
 //
 // Created by dick on 3/22/2018.
 //
-
+#include "game.h"
