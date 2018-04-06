decc671d4043566d89da87bf458d2609d6a8fdbe
Steven Medina <stevenmedina@ufl.edu>
2018-04-06 19:21:55 -0400

    Merge branch 'master' of C:\Users\NightBot\CLionProjects\DJJAM with conflicts.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 //finally got this working; only took 10 hours!
 //learn about time.h; apparently that's a thing
+//obviously learn about SFML, because FML if we use OpenGL
 
 #include <SFML/Graphics.hpp>
 #include<iostream>
@@ -8,22 +9,20 @@
 
 //this includes the standard functions in SFML's custom namespace which saves us an assload of "sf::insertCrapHere"
 using namespace sf;
-float score = 0.f;
 //who uses structs anymore? I guess C is useful; probably better to do this in a class; just declaring coordinates
 
 int main()
 {
-    std::cout<< "";
+    std::cout<< "Michael you're famous";
     //renders a window of size whatever I've declared; check parameters
     RenderWindow app(VideoMode(400, 533), "DJJAM");
     //sets framerate maximum to stop the program from seizing
     app.setFramerateLimit(60);
 
-
     //loads textures from files, duhhh
     Texture t1,t2;
-    t1.loadFromFile("../cmake_modules/Images/background.png");
-    t2.loadFromFile("../cmake_modules/Images/platform.png");
+    t1.loadFromFile("C:/Users/miami/CLionProjects/untitled/images/background.png");
+    t2.loadFromFile("C:/Users/miami/CLionProjects/untitled/images/platform.png");
 
     Player player;
     //declaring the different images for their respective parts in the game (the person, platform, and background)
@@ -62,54 +61,35 @@ int main()
         //nice built in functions to detect key movement; ya love to see it!
         if (Keyboard::isKeyPressed(Keyboard::Right)) {
             currentSprite = sPersR;
-            if (x>380) {
-                x = -60;
-            }else {
-                x += 3.5;
-            }
+            x += 3;
         }
         if (Keyboard::isKeyPressed(Keyboard::Left)) {
             currentSprite = sPersL;
-            if (x<-60) {
-                x = 410;
-            } else {
-                x -= 3.5;
-            }
+            x-=3;
         }
 
         //good ol' gravity
-        dy+=0.25;
+        dy+=0.2;
         //changes the current position to account for movement
         y+=dy;
         if (y>500)  dy=-10;
+
         if (y<h)
-            for (int i=0;i<10;i++) {
+            for (int i=0;i<10;i++)
+            {
                 y=h;
-                score += .01;
                 plat[i].y=plat[i].y-dy;
                 if (plat[i].y>533) {plat[i].y=0; plat[i].x=rand()%400;}
             }
 
         for (int i=0;i<10;i++)
             if ((x+50>plat[i].x) && (x+20<plat[i].x+68)
-                && (y+70>plat[i].y) && (y+70<plat[i].y+14) && (dy>0))  dy=-11;
+                && (y+70>plat[i].y) && (y+70<plat[i].y+14) && (dy>0))  dy=-10;
 
         currentSprite.setPosition(x,y);
 
         app.draw(sBackground);
         app.draw(currentSprite);
-
-        sf::Font m_font;
-        m_font.loadFromFile("../cmake_modules/Images/OpenSans-Regular.ttf");
-        sf::Text m_score;
-        m_score.setFont(m_font);
-        m_score.setCharacterSize(35);
-        m_score.setFillColor(sf::Color::Black);
-        m_score.setPosition(10,0);
-
-        m_score.setString("score: " + std::to_string((int)score));
-        app.draw(m_score);
-
         for (int i=0;i<10;i++)
         {
             sPlat.setPosition(plat[i].x,plat[i].y);
